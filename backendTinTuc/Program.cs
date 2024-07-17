40fe627bb0bd825f2cb3c4dc195d56096de53873
ADMIN <ADMIN@LAPTOP-2A7AGG39>
2024-07-17 20:06:09 +0700

    cc

--- backendTinTuc/Program.cs
+++ backendTinTuc/Program.cs
@@ -12,7 +12,7 @@ builder.Services.AddCors(options =>
                       policy =>
                       {
                           policy.WithOrigins("localhost:3000",
-                                              "localhost:3001");
+                                              "hhjhjhjhjhj");
                       });
 });
 builder.Services.AddMvc();
